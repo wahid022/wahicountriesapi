c580c5b911a333dd816d3357603e96de0cc9d333
wahid022 <rahmanwahid022@gmail.com>
2024-10-05 13:04:28 +0530

    Changed the heading

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header className={`header-container ${isDark? 'dark': ''}`}>
       <div className="header-content">
         <h2 className="title">
-          <a href="/">Where in the world?</a>
+          <a href="/">Where in the worlds?</a>
         </h2>
         <p className="theme-changer" onClick={() => {
           setIsDark(!isDark)
